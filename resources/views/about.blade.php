1025c4a4ab08b28a6e569626d52dddc7e7f9fbca
Rugie Ann Barrameda <norugie015@gmail.com>
2019-08-20 18:39:56 -0700

    Basic Routing

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'About Us')
+
+@section('content')
+
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo erat, tristique a massa et, viverra dapibus odio. Donec metus nisl, vestibulum maximus interdum quis, interdum vitae diam. Nulla tempor sapien non pretium efficitur. Suspendisse congue justo risus. In sit amet velit justo. Morbi blandit enim vel sodales sollicitudin. Proin volutpat odio ex, eu tempus sapien finibus eu. Fusce eget dictum arcu. Fusce sed tortor tempus, tempor felis vel, iaculis libero.</p>
+@endsection
